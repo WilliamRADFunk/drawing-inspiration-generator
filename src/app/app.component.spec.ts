15aab3ca55aac677dd9aa1cc24cdd5466c63f1de
WilliamRADFunk <contact@WILLIAMRADFUNK.com>
2020-01-28 10:07:00 -0800

    Fixes build pipeline.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,23 @@
 import { TestBed, async } from '@angular/core/testing';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { WordPickerService } from './services/word-picker.service';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        NgbModule,
+        HttpClientModule,
+        RouterModule.forRoot([], {useHash: true})
+      ],
       declarations: [
         AppComponent
       ],
+      providers: [ WordPickerService ],
     }).compileComponents();
   }));
 
@@ -14,18 +25,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'drawing-inspiration-generator'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('drawing-inspiration-generator');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('drawing-inspiration-generator app is running!');
   });
 });
--- src/app/services/word-picker.service.spec.ts
+++ src/app/services/word-picker.service.spec.ts
@@ -1,9 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { WordPickerService } from './word-picker.service';
 
 describe('WordPickerService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule,
+        RouterModule.forRoot([], {useHash: true})
+      ]
+  }));
 
   it('should be created', () => {
     const service: WordPickerService = TestBed.get(WordPickerService);
